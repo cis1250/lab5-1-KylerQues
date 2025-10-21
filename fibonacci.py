98c20ee5791b67e467e4ecdb32114090c95c412c
Kyler <quesnelk@uoguelph.ca>
2025-10-21 10:47:13 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,15 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+running = true
+while running = true:
+  terms = input("Enter number of terms: ")
+  if terms > 0:
+    print("Calculating first " + terms + " terms of fibonacci sequence")
+    for x in range(terms):
+      for y in range(1, terms):
+        num = x
+        num2 = y + x
+        print(num)
+        print(num2)
+      
