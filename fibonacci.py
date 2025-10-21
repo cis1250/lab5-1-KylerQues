9e9b4925d067700e9d9ab3c26d4b9ddf8b3f68ef
Kyler <quesnelk@uoguelph.ca>
2025-10-21 11:51:50 -0400

    Update fibonacci.py
    Refactored Fibonacci sequence calculation into functions for better structure and readability.

--- fibonacci.py
+++ fibonacci.py
@@ -2,11 +2,29 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
-running = True
-while running == True:
-  terms = input("Enter number of terms: ")
-  if int(terms) > 0:
-    print("Calculating first " + terms + " terms of fibonacci sequence")
-    for n in range(int(terms)):
-      fib = round((((1 + 5**0.5)/2)**n - ((1 - 5**0.5)/2)**n) / (5**0.5)) # Binet's formula
-      print(fib)
+def main():
+    
+
+  def get_terms():
+      while True:
+          terms = input("Enter number of terms: ")
+          if terms.isdigit() and int(terms) > 0:
+              return int(terms)
+          else:
+              print("Please enter a positive integer.")
+  def calculate(terms):
+      nums = []
+      for n in range(terms):
+          fib = round((((1 + 5**0.5)/2)**n - ((1 - 5**0.5)/2)**n) / (5**0.5)) # Binet's formula
+          nums.append(fib)
+      return nums
+
+  def print_out(output):
+      print("Fibonacci sequence:")
+      for i in output:
+          print(i)
+
+  terms = get_terms()
+  output = calculate(terms)
+  print_out(output)
+main()
