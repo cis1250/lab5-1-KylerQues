41011cf7c6696371c57d0e0c5aa42c0e6125f9d4
Kyler <quesnelk@uoguelph.ca>
2025-10-21 10:59:08 -0400

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,15 +2,11 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
-running = true
-while running = true:
+running = True
+while running == True:
   terms = input("Enter number of terms: ")
-  if terms > 0:
+  if int(terms) > 0:
     print("Calculating first " + terms + " terms of fibonacci sequence")
-    for x in range(terms):
-      for y in range(1, terms):
-        num = x
-        num2 = y + x
-        print(num)
-        print(num2)
-      
+    for n in range(int(terms)):
+      fib = round((((1 + 5**0.5)/2)**n - ((1 - 5**0.5)/2)**n) / (5**0.5)) # Binet's formula
+      print(fib)
